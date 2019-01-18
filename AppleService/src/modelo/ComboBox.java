4053335be6d79e743f695822bcfd6a58ac7ee971
Matias Jara <mjara.figueroa@gmail.com>
2019-01-18 10:42:46 -0300

    Ejercicio Apple Service

---
+++ AppleService/src/modelo/ComboBox.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author mjara
+ */
+public class ComboBox {
+    
+    private int id;
+    private String nombre;
+
+    public ComboBox(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+    
+}
